2ffdb16cb612c571991b9832076a32fa4a8d7590
jongse7 <mmssjj72@naver.com>
2026-07-25 01:43:31 +0900

    feat: 믹스패널 리플레이와 홈 미션 카테고리 보강

--- apps/web/app/(tabs)/constants/home.ts
+++ apps/web/app/(tabs)/constants/home.ts
@@ -1,7 +1,5 @@
 import { MISSION_CATEGORIES, type MissionCategory } from "../mission/constants/mission";
 
-export type HomeMissionCategory = Exclude<MissionCategory, "취미">;
+export type HomeMissionCategory = MissionCategory;
 
-export const HOME_MISSION_CATEGORIES = MISSION_CATEGORIES.filter(
-  (category): category is HomeMissionCategory => category !== "취미",
-);
+export const HOME_MISSION_CATEGORIES = MISSION_CATEGORIES;
